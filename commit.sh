146c4339719a35b5d7482f7998d855d6e70af8ca
yukia18 <abe@ailab.ics.keio.ac.jp>
2020-09-25 11:19:22 +0900

    [Remove] requirement and commit.sh

--- commit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-start_time=`date +%s`
-jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute --inplace $1
-end_time=`date +%s`
-elapsed_time=$(($end_time - $start_time))
-echo "Complete at `TZ=JST-9 date` ($elapsed_time sec)".
